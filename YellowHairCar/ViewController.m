fb2afc0ac2233687a7234b5179f1a14cdc06fb4f
XuWenYan <xuwenyan@wfydemacbook-pro.local>
2019-01-03 18:33:40 +0800

    测试

--- YellowHairCar/ViewController.m
+++ YellowHairCar/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 
