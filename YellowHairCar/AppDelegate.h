1f791f5fb348c3376886559ecce59db3e991d23a
XuWenYan <xuwenyan@wfydemacbook-pro.local>
2019-01-03 16:42:48 +0800

    Initial Commit

---
+++ YellowHairCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YellowHairCar
+//
+//  Created by XuWenYan on 2019/1/3.
+//  Copyright © 2019年 XChuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YellowHairCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YellowHairCar
+//
+//  Created by XuWenYan on 2019/1/3.
+//  Copyright © 2019年 XChuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
